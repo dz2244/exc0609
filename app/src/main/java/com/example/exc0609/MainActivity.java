c5398f98d4363717cd05ed102dc0ad75135ded68
dz2244 <dz2244@bs.amalnet.k12.il>
2024-02-17 13:53:48 +0200

    commit 3

--- app/src/main/java/com/example/exc0609/MainActivity.java
+++ app/src/main/java/com/example/exc0609/MainActivity.java
@@ -1,18 +1,38 @@
 package com.example.exc0609;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    EditText et;
+    WebView wv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn.findViewById(R.id.btn);
+        et.findViewById(R.id.et);
+        wv.findViewById(R.id.wv);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setWebViewClient(new MyWebViewClient());
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 
     public void ace(View view) {
+        String str = et.getText().toString();
+        wv.loadUrl(str);
     }
 }
+
